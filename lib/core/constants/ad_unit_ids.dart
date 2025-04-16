d158a17d77f6b25a54c6527a0c8e3c2e55213cf0
Chayan Mistry <chayanmistrry@gmail.com>
2025-04-17 00:07:35 +0600

    version 1.5.0

--- lib/core/constants/ad_unit_ids.dart
+++ lib/core/constants/ad_unit_ids.dart
@@ -1,7 +1,7 @@
 class AdUnitIds {
   // Google Ads
-  static const googleInterstitial = "ca-app-pub-3224119074707344/4058225198";
-  static const googleBanner = "ca-app-pub-3224119074707344/1274582513";
+  static const googleInterstitial = "ca-app-pub-8398893250226653/3570473099";
+  static const googleBanner = "ca-app-pub-8398893250226653/4331647156";
 
   // AppLovin Ads
   static const appLovinInterstitial = "e2a564b95db03499";
